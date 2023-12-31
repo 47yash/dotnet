8268ea219030339bab2ec0bf5628648b996ac0a0
47yash <yashsher47@gmail.com>
2023-12-31 16:58:24 +0530

    c

---
+++ BOL/Artice.cs
@@ -0,0 +1,7 @@
+namespace BOL;
+public class Article{
+    int Id{get;set;};
+    string name{get;set;};
+    string author{get;set;};
+
+}
---
+++ DAL/DBmanager.cs
@@ -0,0 +1,10 @@
+﻿namespace DAL;
+
+public DBManager
+{
+    public static string conString=@"server=localhost;port=3306;user=root;password=Sher@360;database=practice";
+    public static list<Article> art=new list<Article>;
+    MySqlConnection con=new MySqlConnection();
+    con
+
+}
