2eb8bd14b373a004bcaf3d5ff1bee3dc8e3d9181
47yash <144767088+47yash@users.noreply.github.com>
2024-01-05 20:41:32 +0530

    Add files via upload

---
+++ 2/BLL/StudentManager.cs
@@ -0,0 +1,29 @@
+﻿namespace BLL;
+using BOL;
+using DAL;
+using System.Collections.Generic;
+public class StudentManager
+{
+    public List<Student> GetStudent(){
+        List<Student> slist=new List<Student>();
+        slist=DBmanager.GetStudent();
+        return slist;
+        
+
+    }
+
+    public bool DeleteStudent(int id){
+        Console.WriteLine(id);
+      
+       return DBmanager.DeleteStudent(id);
+
+    }
+    public bool InsertStudent(int id,string name,int roll){
+        return DBmanager.InsertStudent(id,name,roll);
+    }
+
+    public bool UpdateStudent(int id,string name,int roll){
+        return DBmanager.InsertStudent(id,name,roll);
+    }
+
+}
